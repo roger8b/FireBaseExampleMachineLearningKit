13142503ac9a1ef596c2931d882f5015b608d6e3
Roger Martins <roger.martins@madeinweb.com.br>
2018-09-30 20:43:57 -0300

    Remodelagem do layout Adicionado suporte a camera do aparelho

--- app/src/main/java/com/example/rm/firebaseexamplemachinelearningkit/MainActivity.kt
+++ app/src/main/java/com/example/rm/firebaseexamplemachinelearningkit/MainActivity.kt
@@ -1,33 +1,34 @@
 package com.example.rm.firebaseexamplemachinelearningkit
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.media.ExifInterface
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
 import android.view.inputmethod.EditorInfo
-import com.example.rm.firebaseexamplemachinelearningkit.R.id.image_holder
-import com.example.rm.firebaseexamplemachinelearningkit.R.id.image_url_field
+import android.widget.Toast
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.squareup.picasso.Picasso
-import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
-import org.jetbrains.anko.ctx
-import org.jetbrains.anko.doAsync
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_IMAGE_CAPTURE = 100
+    private val REQUEST_CAMERA_PERMISSION = 200
+    private lateinit var uri: Uri
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,20 +55,81 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadControls() {
         bt_text.setOnClickListener {
-            recognizeText(it)
+            recognizeText()
         }
 
         bt_face.setOnClickListener {
-            detectFaces(it)
+            detectFaces()
         }
 
         bt_label.setOnClickListener {
-            generateLabels(it)
+            generateLabels()
+        }
+
+        bt_take_picture.setOnClickListener {
+            loadCamera()
         }
     }
 
-    fun recognizeText(v: View) {
+    private fun loadCamera() {
+        val isAllowed = isAllowedToUseACamera()
+        if(isAllowed){
+            startCamera()
+        }
+    }
 
+    private fun isAllowedToUseACamera() : Boolean {
+        val checkSelfPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val permission = arrayOf(Manifest.permission.CAMERA)
+        ActivityCompat.requestPermissions(this, permission, REQUEST_CAMERA_PERMISSION)
+        if (checkSelfPermission != PackageManager.PERMISSION_GRANTED)
+        {
+            return  true
+        }
+
+        return  false
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        val isAllowed = requestCode == REQUEST_CAMERA_PERMISSION && grantResults.isNotEmpty()
+
+        if(isAllowed){
+            startCamera()
+        } else {
+            Toast.makeText(this,"Habilite o acesso a camera", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startCamera() {
+        val externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val authority = "com.example.rm.firebaseexamplemachinelearningkit.fileprovider"
+        val createTempFile = File.createTempFile("picture", ".jpg", externalFilesDir)
+        uri = FileProvider.getUriForFile(this, authority, createTempFile)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply { putExtra(MediaStore.EXTRA_OUTPUT, uri) }
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val resultOk = resultCode == Activity.RESULT_OK
+        val requestImageCapture = requestCode == REQUEST_IMAGE_CAPTURE
+        if(requestImageCapture && resultOk){
+            val bitmap = getCapturedImage(uri)
+            image_holder.setImageBitmap(bitmap)
+        }
+    }
+
+    private fun getCapturedImage(uri: Uri): Bitmap {
+        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+        return when (ExifInterface(contentResolver.openInputStream(uri)).getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> Bitmap.createBitmap(bitmap,0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(90F) }, true)
+            ExifInterface.ORIENTATION_ROTATE_180 -> Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(180F) }, true)
+            ExifInterface.ORIENTATION_ROTATE_270 -> Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(270F) }, true)
+            else -> bitmap
+        }
+    }
+
+
+    fun recognizeText() {
         val textImage = FirebaseVisionImage.fromBitmap(
                 (image_holder.drawable as BitmapDrawable).bitmap)
         val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
@@ -85,7 +147,7 @@ class MainActivity : AppCompatActivity() {
         detector.close()
 }
 
-fun detectFaces(v: View) {
+fun detectFaces() {
     val detector = FirebaseVision.getInstance().visionFaceDetector
     detector.detectInImage(FirebaseVisionImage.fromBitmap(
             (image_holder.drawable as BitmapDrawable).bitmap
@@ -106,7 +168,7 @@ fun detectFaces(v: View) {
     }
 }
 
-fun generateLabels(v: View) {
+fun generateLabels() {
     val detector = FirebaseVision.getInstance().visionCloudLabelDetector
     detector.detectInImage(FirebaseVisionImage.fromBitmap(
             (image_holder.drawable as BitmapDrawable).bitmap
